08c588a8fb62c551d3752f0b16a524558032fa2d
Jordan Sydney-Darling <jordan_sd@outlook.com>
2026-03-05 07:28:45 +0000

    Downloaded datasets

--- task1/train.py
+++ task1/train.py
@@ -6,26 +6,15 @@ from torch.utils.data import DataLoader
 def main():
     print("Downloading CIFAR-10 dataset...")
 
-    datasets.CIFAR10(root="./data", train=True, download=True)
-    datasets.CIFAR10(root="./data", train=False, download=True)
+    transform = transforms.ToTensor()
+
+    train_dataset = datasets.CIFAR10(root="./data",train=True,download=True,transform=transform)
+    test_dataset = datasets.CIFAR10(root="./data",train=False,download=True,transform=transform)
 
     print("Dataset downloaded successfully.")
     print("Loading dataset into PyTorch...")
 
-    transform = transforms.ToTensor()
-
-    train_dataset = datasets.CIFAR10(
-        root="./data",
-        train=True,
-        download=False,
-        transform=transform
-    )
-
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=64,
-        shuffle=True
-    )
+    train_loader = DataLoader(train_dataset,batch_size=64,shuffle=True)
 
     images, labels = next(iter(train_loader))
 
@@ -41,6 +30,7 @@ def main():
     print("Max pixel value:", image.max().item())
 
     classes = train_dataset.classes
+    print("Classes:", classes)
 
 if __name__ == "__main__":
     main()
